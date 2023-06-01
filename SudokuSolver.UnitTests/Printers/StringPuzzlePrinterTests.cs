99301147a13dd18818e280d3c5a9fbc9668915b1
matt <mrlst16@mail.rmu.edu>
2023-06-01 10:55:32 -0700

    add string printer

---
+++ SudokuSolver.UnitTests/Printers/StringPuzzlePrinterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuSolver.Printers;
+using SudokuSolver.Tests.MockData;
+
+namespace SudokuSolver.UnitTests.Printers
+{
+    public class StringPuzzlePrinterTests
+    {
+        [Fact]
+        public void TestPrint()
+        {
+            StringPuzzlePrinter printer = new StringPuzzlePrinter();
+            string result = printer.Print(MockPuzzleFactory.SolvedPuzzle);
+
+        }
+    }
+}
---
+++ SudokuSolver/Printers/StringPuzzlePrinter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuSolver.Interfaces;
+using SudokuSolver.Models;
+
+namespace SudokuSolver.Printers
+{
+    public class StringPuzzlePrinter : IPuzzlePrinter
+    {
+        public string Print(SudokuPuzzle puzzle)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    builder.Append(puzzle[i, j].Value);
+                    builder.Append(' ');
+                }
+                builder.Append('\n');
+            }
+            return builder.ToString();
+        }
+    }
+}
