5eaa71746e69aaa96c3f96e92e8ddd09ef7fb70c
matt <mrlst16@mail.rmu.edu>
2023-05-31 20:55:08 -0700

    put back IsInRange for cellclass

--- SudokuSolver/SudokuExtensions.cs
+++ SudokuSolver/SudokuExtensions.cs
@@ -1,8 +1,13 @@
-﻿namespace SudokuSolver
+﻿using SudokuSolver.Models;
+
+namespace SudokuSolver
 {
     public static class SudokuExtensions
     {
         public static bool IsInRange(this int x)
             => x is >= 1 and <= 9;
+
+        public static bool IsInRange(this Cell cell)
+            => cell.Value.IsInRange();
     }
 }
