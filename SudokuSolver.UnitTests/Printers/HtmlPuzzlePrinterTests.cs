f7f35c087e515cf94a9af51c546b5c9dbb58bdc6
matt <mrlst16@mail.rmu.edu>
2023-06-01 17:15:59 -0700

    add html printer

---
+++ SudokuSolver.UnitTests/Printers/HtmlPuzzlePrinterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuSolver.Helpers;
+using SudokuSolver.Models;
+using SudokuSolver.Printers;
+using SudokuSolver.Tests.MockData;
+
+namespace SudokuSolver.UnitTests.Printers
+{
+    public class HtmlPuzzlePrinterTests
+    {
+        [Fact]
+        public void Test()
+        {
+            HtmlPuzzlePrinter printer = new HtmlPuzzlePrinter();
+            SudokuPuzzle puzzle = MockPuzzleFactory.EasyLevelPuzzle1;
+            FastPencil.Apply(puzzle);
+            string result = printer.Print(puzzle);
+        }
+    }
+}
---
+++ SudokuSolver/Printers/HtmlPuzzlePrinter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuSolver.Interfaces;
+using SudokuSolver.Models;
+
+namespace SudokuSolver.Printers
+{
+    public class HtmlPuzzlePrinter : IPuzzlePrinter
+    {
+        public string Print(SudokuPuzzle puzzle)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("<table style=\"border: 1px solid black\"; border-collapse: collapse;>");
+            for (int i = 0; i < 9; i++)
+            {
+                builder.AppendLine("<tr style=\"border: 1px solid black\"; border-collapse: collapse;>");
+                for (int j = 0; j < 9; j++)
+                {
+                    builder.AppendLine("<td style=\"border: 1px solid black\"; border-collapse: collapse;>");
+                    
+                    Cell cell = puzzle[i, j];
+                    if (cell.Value.IsInRange())
+                    {
+                        builder.Append($"<b>{cell.Value}</b>");
+                    }
+                    else
+                    {
+                        string possibilities = Possibilities(cell);
+                        builder.Append(possibilities);
+                    }
+                    builder.AppendLine("</td>");
+                }
+
+                builder.AppendLine("</tr>");
+            }
+            builder.AppendLine("</table>");
+            return builder.ToString();
+        }
+
+        private string Possibilities(Cell cell)
+        {
+            StringBuilder builder = new();
+            int place = 1;
+
+            builder.AppendLine("<table>");
+            for (int i = 0; i < 3; i++)
+            {
+                builder.AppendLine("<tr>");
+                for (int j = 0; j < 3; j++)
+                {
+                    builder.AppendLine("<td>");
+                    if (cell.Possiblities.Contains(place))
+                        builder.Append(place);
+                    builder.AppendLine("</td>");
+                    place++;
+                }
+
+                builder.AppendLine("</tr>");
+            }
+            builder.AppendLine("</table>");
+            return builder.ToString();
+        }
+    }
+}
